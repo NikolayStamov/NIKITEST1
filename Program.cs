c13af5472ac592479560c1cf1d03a2302e2a1dba
NikolayStamov <38375318+NikolayStamov@users.noreply.github.com>
2018-04-14 13:19:54 +0300

    AddetoDO

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0}",Math.Round(a * h /2,2));
+
+        }
+    }
+}
